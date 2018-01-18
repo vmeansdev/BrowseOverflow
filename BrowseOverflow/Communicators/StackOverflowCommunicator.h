439ea3df976ba3ada24a0a0873be4f33c71fbb2b
vmeansdev <vmeansdev@gmail.com>
2018-01-19 00:07:55 +0300

    ADD: communicators

---
+++ BrowseOverflow/Communicators/StackOverflowCommunicator.h
@@ -0,0 +1,15 @@
+//
+//  StackOverflowCommunicator.h
+//  BrowseOverflow
+//
+//  Created by vmeansdev on 18/01/2018.
+//  Copyright © 2018 vmeansdev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StackOverflowCommunicator : NSObject
+
+- (void)searchForQuestionsWithTag:(NSString *)tag;
+
+@end
---
+++ BrowseOverflow/Communicators/StackOverflowCommunicator.m
@@ -0,0 +1,13 @@
+//
+//  StackOverflowCommunicator.m
+//  BrowseOverflow
+//
+//  Created by vmeansdev on 18/01/2018.
+//  Copyright © 2018 vmeansdev. All rights reserved.
+//
+
+#import "StackOverflowCommunicator.h"
+
+@implementation StackOverflowCommunicator
+
+@end
