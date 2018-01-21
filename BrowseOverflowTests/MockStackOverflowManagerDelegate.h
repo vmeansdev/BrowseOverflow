3b234cf12a48524e0b49fe38888b50cd85edc0a9
vmeansdev <vmeansdev@gmail.com>
2018-01-21 20:04:03 +0300

    ADD: MockStackOverflowManagerDelegate recreated

---
+++ BrowseOverflowTests/MockStackOverflowManagerDelegate.h
@@ -0,0 +1,14 @@
+//
+//  MockStackOverflowManagerDeleagte.h
+//  BrowseOverflowTests
+//
+//  Created by vmeansdev on 21/01/2018.
+//  Copyright © 2018 vmeansdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "StackOverflowManagerDelegate.h"
+
+@interface MockStackOverflowManagerDeleagte : StackOverflowManagerDelegate
+
+@end
---
+++ BrowseOverflowTests/MockStackOverflowManagerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MockStackOverflowManagerDeleagte.m
+//  BrowseOverflowTests
+//
+//  Created by vmeansdev on 21/01/2018.
+//  Copyright © 2018 vmeansdev. All rights reserved.
+//
+
+#import "MockStackOverflowManagerDeleagte.h"
+
+@implementation MockStackOverflowManagerDeleagte
+
+@end
